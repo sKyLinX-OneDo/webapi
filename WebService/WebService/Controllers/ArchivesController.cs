e598e7ee68189f25691a23ae7897623a12001fd9
sKyLinX <542104961@qq.com>
2017-06-27 13:08:58 +0800

    webservice

---
+++ WebService/WebService/Controllers/ArchivesController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Models;
+
+namespace WebService.Controllers
+{
+    public class ArchivesController : ApiController
+    {
+
+        public string Get()
+        {
+            return "test";
+        }
+
+        [HttpGet]
+        public string Get(string id)
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = Config.ConnectionString,
+                DbType = DbType.SqlServer,
+                IsAutoCloseConnection = true
+            });
+
+            var getSome = db.Queryable<Archives>().Where(it => it.ID == id).ToList();
+
+            return JsonConvert.SerializeObject(getSome);
+
+        }
+
+    }
+}
---
+++ WebService/WebService/Models/Archives.cs
@@ -0,0 +1,26 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Models
+{
+    [SugarTable("Record")]
+    public class Archives
+    {
+        public string Name { get; set; }
+
+        [SugarColumn(ColumnName="IdentityCard")]
+        public string ID { get; set; }
+
+        [SugarColumn(ColumnName = "PersonalCode_Social")]
+        public string SocialCode { get; set; }
+
+        public string CompanyName { get; set; }
+
+        [SugarColumn(ColumnName ="Remark_1")]
+        public string Remark { get; set; }
+
+    }
+}
